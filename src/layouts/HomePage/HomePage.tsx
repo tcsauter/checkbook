56b7d1c8d569d9f29c31b721183cc72fead1a6cf
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-04-30 19:02:17 -0400

    Begin work on AccountsSummary.

--- src/layouts/HomePage/HomePage.tsx
+++ src/layouts/HomePage/HomePage.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import {SpentListItem} from "./components/SpentListItem";
+import {AccountsSummary} from "./components/AccountsSummary";
 
 export const HomePage: React.FC<{ name: string }> = (props) => {
-    return(
-        <div className='container'>
-            <ul className='list-group' >
-                <SpentListItem amount={145.36} date={'4/30/2023'} name={"Citi"} />
-                <SpentListItem amount={4.35} date={'4/30/2023'} name={"BoA"} />
-            </ul>
-        </div>
+    return (
+        <>
+            <div className='container' id='accountsSummaryContainer'>
+                <AccountsSummary name={"Travis Sauter-Hunsberger"}/>
+            </div>
+            <div className='container' id='spentMoneyContainer'>
+                <ul className='list-group'>
+                    <SpentListItem amount={145.36} date={'4/30/2023'} name={"Citi"}/>
+                    <SpentListItem amount={4.35} date={'4/30/2023'} name={"BoA"}/>
+                </ul>
+            </div>
+        </>
     );
 }
---
+++ src/layouts/HomePage/components/AccountsSummary.tsx
@@ -0,0 +1,7 @@
+export const AccountsSummary: React.FC<{ name: string }> = (props) => {
+    return (
+        <>
+            <h5>Accounts summary for <b>{props.name}</b></h5>
+        </>
+    );
+}

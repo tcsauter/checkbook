d658374672aaf87c14b99cec79096a0fd1691a5e
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-05-04 18:19:55 -0400

    Create and style RemainCard.

--- src/layouts/HomePage/HomePage.tsx
+++ src/layouts/HomePage/HomePage.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import {SpentListItem} from "./components/SpentListItem";
-import {AccountsSummary} from "./components/AccountsSummary";
+import {AccountsSummaryCard} from "./components/AccountsSummaryCard";
+import {RemainCard} from "./components/RemainCard";
 
 export const HomePage: React.FC<{ name: string }> = (props) => {
     return (
-        <>
-            <div className='container d-flex'>
-                <div className='card bg-info mt-5' id='accountsSummaryContainer'>
-                    <AccountsSummary name={"Travis Sauter-Hunsberger"}/>
-                </div>
-                <div id='spentMoneyContainer'>
-                    <h5>Details</h5>
-                    <ul >
-                        <SpentListItem amount={145.36} date={'4/30/2023'} name={"Citi"}/>
-                        <SpentListItem amount={4.35} date={'4/30/2023'} name={"BoA"}/>
-                    </ul>
-                </div>
+        <div className='container d-flex mt-3'>
+            <div className='container'>
+                <RemainCard name={""}/>
+                <AccountsSummaryCard name={"Travis Sauter-Hunsberger"}/>
             </div>
-        </>
+            <div id='spentMoneyContainer'>
+                <h5>Details</h5>
+                <ul>
+                    <SpentListItem amount={145.36} date={'4/30/2023'} name={"Citi"}/>
+                    <SpentListItem amount={4.35} date={'4/30/2023'} name={"BoA"}/>
+                </ul>
+            </div>
+        </div>
     );
 }
---
+++ src/layouts/HomePage/components/RemainCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const RemainCard: React.FC<{ name: string }> = (props) => {
+    return (
+        <div className='card bg-info text-light' id='remainContainer'>
+            <div className='card-body'>
+                <h5 className='card-title'>Remaining Amount</h5>
+                <p className='card-text'>$435.00</p>
+            </div>
+        </div>
+    );
+}

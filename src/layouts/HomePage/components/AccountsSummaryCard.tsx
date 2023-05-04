4cf640f33eebc245eef0393857c0511fefdac1a2
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-05-04 18:18:59 -0400

    Rename AccountSummary to AccountSummaryCard.

---
+++ src/layouts/HomePage/components/AccountsSummaryCard.tsx
@@ -0,0 +1,22 @@
+import {AcctSummaryListItem} from "./AcctSummaryListItem";
+
+export const AccountsSummaryCard: React.FC<{ name: string }> = (props) => {
+    return (
+        <div className='card bg-info mt-1' id='accountsSummaryContainer'>
+            <div className='card-body text-light'>
+                <h5 className='card-title'>Summary by Account</h5>
+                <div>
+                    <ul className='list-group mt-4'>
+                        <AcctSummaryListItem acctName={"Bank of America"} amt={"110.33"}/>
+                        <AcctSummaryListItem acctName={"Citi"} amt={"45.00"}/>
+                    </ul>
+                    <hr/>
+                    <div className='d-flex justify-content-between'>
+                        <h5 className='card-text me-5'><b>Total:</b></h5>
+                        <p className='card-text'>$353.46</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}

0431009b9d35460a5638ccce695adf4b6d4b2d9d
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-05-08 19:00:18 -0400

    Begin to make SpentListItem updateable.

--- src/layouts/HomePage/components/DetailsCard.tsx
+++ src/layouts/HomePage/components/DetailsCard.tsx
@@ -7,8 +7,24 @@ export const DetailsCard: React.FC<{name: string}> = (props) => {
             <div className='card-body'>
                 <h5 className='card-title'>Details</h5>
                 <ul className='list-group'>
-                    <SpentListItem amount={145.36} date={'4/30/2023'} name={"Citi"}/>
-                    <SpentListItem amount={4.35} date={'4/30/2023'} name={"Bank of America"}/>
+                    <SpentListItem input={{
+                        amount: 145.36,
+                        date: {
+                            month: 4,
+                            day: 30,
+                            year: 2023
+                        },
+                        account: "Citi"
+                    }} />
+                    <SpentListItem input={{
+                        amount: 4.35,
+                        date: {
+                            month: 4,
+                            day: 30,
+                            year: 2023
+                        },
+                        account: "Bank of America"
+                    }} />
                 </ul>
             </div>
         </div>
--- src/layouts/HomePage/components/SpentListItem.tsx
+++ src/layouts/HomePage/components/SpentListItem.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
-export const SpentListItem: React.FC<{ amount: number, date: string, name: string }> = (props) => {
+interface SpentListItemProps {
+    amount: number,
+    date: {
+        month: number,
+        day: number,
+        year: number;
+    },
+    account: string;
+}
 
+export const SpentListItem: React.FC<{ input: SpentListItemProps }> = (props) => {
+    const [showAmtInput, setShowAmtInput] = useState(false);
+    const [amtInputValue, setAmtInputValue] = useState('');
+
+    function processAmtInput(event: React.KeyboardEvent<HTMLInputElement>) {
+        if(event.key === "Enter"){
+            const inputValue: number = Number(amtInputValue);
+
+            if(isNaN(inputValue)) {
+
+            }else{
+                setShowAmtInput(false);
+
+                //todo: Think more deeply about the flow of data and how individual transactions can be updated safely
+            }
+        }
+    }
 
     return (
         <div className='list-item
@@ -12,10 +37,31 @@ export const SpentListItem: React.FC<{ amount: number, date: string, name: strin
                         border-bottom'
         >
             <div>
-                <h5 className='me-5'>${props.amount}</h5>
-                <small>{props.date}</small>
+                <h5 className='me-5'>
+                    {props.input.amount.toLocaleString("en-US", {
+                        style: "currency",
+                        currency: "USD"
+                    })}
+                </h5>
+                <div className={showAmtInput ? 'input-group' : 'd-none input-group mb-3'}
+                     id='amt-entry'
+                >
+                    <span className="input-group-text" id="basic-addon1">$</span>
+                    <div className="form-floating">
+                        <input type="text"
+                               className="form-control"
+                               id="amt-input"
+                               placeholder="Amount Input"
+                               value={amtInputValue}
+                               onKeyUp={event => processAmtInput(event)}
+                               onChange={event => setAmtInputValue((event.target as HTMLInputElement).value)}
+                        />
+                    </div>
+                </div>
+
+                <small>{props.input.date.month}/{props.input.date.day}/{props.input.date.year}</small>
             </div>
-            <p>{props.name}</p>
+            <p>{props.input.account}</p>
         </div>
     );
 }

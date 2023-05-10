7d630716cd90d2c94a4e608f3a0190b443027be2
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-05-10 17:14:31 -0400

    Add accounts and expenses mock data.

--- src/models/AccountModel.tsx
+++ src/models/AccountModel.tsx
@@ -1,14 +1,17 @@
 class AccountModel {
     id: number;
     name: string;
+    type: string;
     lastFour?: number;
 
     constructor (id: number,
                  name: string,
+                 type: string,
                  lastFour?: number
     ) {
         this.id = id;
         this.name = name;
+        this.type = type;
         this.lastFour = lastFour ? lastFour : undefined;
     }
 }

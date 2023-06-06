ff2d019a60558372f0623dd10b648863d4b4aeec
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-06-06 09:49:08 -0400

    Get Accounts from the backend.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {HomePage} from "./layouts/HomePage/HomePage";
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import AccountModel from "./models/AccountModel";
-import _accounts from "./resources/data/accounts.json";
 
 function App() {
-    const [accounts, setAccounts] = useState<AccountModel[]>(_accounts as AccountModel[]);
+    const [accounts, setAccounts] = useState<AccountModel[]>([]);
+
+    useEffect(() => {
+        async function getAccounts() {
+            const accounts: AccountModel[] = [];
+            await fetch("http://192.168.1.135:8080/get/accounts")
+                .then(async response => {
+                    if(!response.ok){
+                        console.log(response.statusText);
+                    }
+
+                    const responseJson = await response.json();
+
+                    for(const key in responseJson){
+                        accounts.push({
+                            id: responseJson[key]._id,
+                            name: responseJson[key].name,
+                            type: responseJson[key].type,
+                            lastFour: responseJson[key].lastFour
+                        })
+                    }
+                })
+                .catch(reason => console.log(reason))
+
+            return accounts;
+        }
+
+        getAccounts().then(response => setAccounts(response)).catch(reason => console.log(reason));
+    }, [])
 
     function getAccountNameById(id: string): string | undefined {
         return accounts.find(element => element.id === id)?.name;

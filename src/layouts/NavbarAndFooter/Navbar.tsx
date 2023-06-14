ecefefa100a5c0bb037e8748d14f679142d1651f
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-06-14 05:31:53 -0400

    Add budget period button to Navbar.

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -10,10 +10,17 @@ export const Navbar: React.FC = () => {
                         aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
                     <div className='navbar-nav'>
                         <a className='nav-link active' aria-current='page' href='#'>Home</a>
                         <a className='nav-link' href='#'>Bills</a>
+                    </div>
+                    <div>
+                        <button className="btn btn-outline-secondary dropdown-toggle" type="button"
+                                data-bs-toggle="dropdown">Budget Period</button>
+                        <ul className="dropdown-menu">
+
+                        </ul>
                     </div>
                 </div>
             </div>

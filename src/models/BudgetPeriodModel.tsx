5cad531d9294c3f49c7c4018047719f344df54a4
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-06-18 14:04:59 -0400

    Add startingAmt to budget periods.

--- src/models/BudgetPeriodModel.tsx
+++ src/models/BudgetPeriodModel.tsx
@@ -3,17 +3,20 @@ class BudgetPeriodModel {
     payDate: string;
     budgetStart: string;
     budgetEnd: string;
+    startingAmt: number;
 
     constructor(
         id: string,
         payDate: string,
         budgetStart: string,
-        budgetEnd: string
+        budgetEnd: string,
+        startingAmt: number
     ) {
         this.id = id;
         this.payDate = payDate;
         this.budgetStart = budgetStart;
         this.budgetEnd = budgetEnd;
+        this.startingAmt = startingAmt;
     }
 }
 

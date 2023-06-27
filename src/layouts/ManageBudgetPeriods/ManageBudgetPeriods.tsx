926c7501c83d2f870cf304638ec058f84f087204
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-06-27 05:42:11 -0400

    Add basic elements of ManageBudgetPeriods page and the card that will house the form to add new budget periods.

---
+++ src/layouts/ManageBudgetPeriods/ManageBudgetPeriods.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ManageBudgetPeriods: React.FC = () => {
+    return(
+        <div className="container bg-black min-vh-100 bg-opacity-75">
+            <div className="card bg-light text-muted shadow">
+                <h5 className="card-title mt-3">Add New Budget Periods</h5>
+
+            </div>
+        </div>
+    );
+}

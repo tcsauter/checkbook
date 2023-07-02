a85c4df7460b41425b09ae283f5fd5f9a603a441
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-07-02 18:39:39 -0400

    Implement function to add new budget period to database and refactored budgetPeriodUtil code.

--- src/utils/budgetPeriodUtil.tsx
+++ src/utils/budgetPeriodUtil.tsx
@@ -1,36 +1,61 @@
 import BudgetPeriodModel from "../models/BudgetPeriodModel";
+import {stringifyDate} from "./dateUtils";
 
 const baseUri = "http://localhost:8080";
 const GET = "/get/budgetperiods";
+const POST = "/add/budgetperiod";
 const PUT = "/update/budgetperiod/"
 
-export async function getBudgetPeriods() {
-    const budgetPeriods: BudgetPeriodModel[] = [];
-    await fetch(`${baseUri}${GET}`)
-        .then(async response => {
-            if (!response.ok) {
-                console.log(response.statusText);
-            }
+async function handleReturnedArrayOfBps(response: Response): Promise<BudgetPeriodModel[]> {
+    const container: BudgetPeriodModel[] = [];
+    if (!response.ok) {
+        console.log(response.statusText);
+    }
 
-            const responseJson = await response.json();
+    const responseJson = await response.json();
 
-            for (const key in responseJson) {
-                budgetPeriods.push({
-                    id: responseJson[key]._id,
-                    payDate: responseJson[key].payDate,
-                    budgetStart: responseJson[key].budgetStart,
-                    budgetEnd: responseJson[key].budgetEnd,
-                    startingAmt: responseJson[key].startingAmt
-                })
-            }
+    for (const key in responseJson) {
+        container.push({
+            id: responseJson[key]._id,
+            payDate: responseJson[key].payDate,
+            budgetStart: responseJson[key].budgetStart,
+            budgetEnd: responseJson[key].budgetEnd,
+            startingAmt: responseJson[key].startingAmt
         })
+    }
 
-    return budgetPeriods;
+    return container;
+}
+
+export function findCurrentBudgetPeriod(bps: BudgetPeriodModel[]) {
+    const dateString = stringifyDate(new Date());
+
+    return bps.find(bp => dateString >= bp.budgetStart && dateString <= bp.budgetEnd);
+}
+
+export async function getBudgetPeriods(): Promise<BudgetPeriodModel[]> {
+    return fetch(`${baseUri}${GET}`)
+        .then(response => handleReturnedArrayOfBps(response));
+}
+
+export async function addBudgetPeriod(bp: BudgetPeriodModel): Promise<BudgetPeriodModel[]> {
+    return fetch(`${baseUri}${POST}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            _id: bp.id,
+            payDate: bp.payDate,
+            budgetStart: bp.budgetStart,
+            budgetEnd: bp.budgetEnd,
+            startingAmt: bp.startingAmt
+        })
+    }).then(response => handleReturnedArrayOfBps(response));
 }
 
 export async function updateBudgetPeriod(bp: BudgetPeriodModel): Promise<BudgetPeriodModel[]> {
-    const budgetPeriods: BudgetPeriodModel[] = []
-    await fetch(`${baseUri}${PUT}${bp.id}`, {
+    return fetch(`${baseUri}${PUT}${bp.id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
@@ -42,24 +67,5 @@ export async function updateBudgetPeriod(bp: BudgetPeriodModel): Promise<BudgetP
             budgetEnd: bp.budgetEnd,
             startingAmt: bp.startingAmt
         })
-    })
-        .then(async response => {
-            if (!response.ok) {
-                console.log(response.statusText);
-            }
-
-            const responseJson = await response.json();
-
-            for (const key in responseJson) {
-                budgetPeriods.push({
-                    id: responseJson[key]._id,
-                    payDate: responseJson[key].payDate,
-                    budgetStart: responseJson[key].budgetStart,
-                    budgetEnd: responseJson[key].budgetEnd,
-                    startingAmt: responseJson[key].startingAmt
-                });
-            }
-        })
-
-    return budgetPeriods;
+    }).then(response => handleReturnedArrayOfBps(response));
 }

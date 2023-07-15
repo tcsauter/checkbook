10d9908beb93d546c223613486acb10f24e31dd1
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-07-15 06:56:13 -0400

    Rewrite main App page so that it uses react-router data api to fetch budget and account information.

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,30 @@
-import React, {useEffect, useState} from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Outlet, defer } from "react-router-dom";
 import './App.css';
-import {HomePage} from "./layouts/HomePage/HomePage";
 import {Navbar} from "./layouts/NavbarAndFooter/Navbar";
 import AccountModel from "./models/AccountModel";
 import BudgetPeriodModel from "./models/BudgetPeriodModel";
-import {getBudgetPeriods, updateBudgetPeriod} from "./utils/budgetPeriodUtil";
+import {findCurrentBudgetPeriod, getBudgetPeriods} from "./utils/budgetPeriodUtil";
 import {getAccounts} from "./utils/accountUtil";
-import {ManageBudgetPeriods} from "./layouts/ManageBudgetPeriods/ManageBudgetPeriods";
+
+export async function loader() {
+    //get budget periods
+    const budgetPeriods: BudgetPeriodModel[] = await getBudgetPeriods();
+    const budgetPeriod: BudgetPeriodModel | undefined = findCurrentBudgetPeriod(budgetPeriods);
+
+    //get accounts
+    const accounts: Promise<AccountModel[]> = getAccounts();
+
+    return defer({ budgetPeriods, budgetPeriod, accounts });
+}
 
 function App() {
-    const [accounts, setAccounts] = useState<AccountModel[]>([]);
-    const [accountsLoading, setAccountsLoading] = useState(true);
-    const [accountsError, setAccountsError] = useState(false);
-    const [accountsErrorString, setAccountsErrorString] = useState("");
-    const [budgetPeriods, setBudgetPeriods] = useState<BudgetPeriodModel[] | undefined>(undefined);
-    const [budgetPeriodsLoading, setBudgetPeriodsLoading] = useState(true);
-    const [budgetPeriodsError, setBudgetPeriodsError] = useState(false);
-    const [budgetPeriod, setBudgetPeriod] = useState<BudgetPeriodModel | undefined>(undefined);
-
-    useEffect(() => {
-        setAccountsLoading(true);
-        setBudgetPeriodsLoading(true);
-        getAccounts()
-            .then(accounts => {
-                setAccounts(accounts);
-                setAccountsLoading(false);
-            })
-            .catch(reason => {
-                setAccountsError(true);
-                setAccountsErrorString(reason.toString());
-            });
-
-        getBudgetPeriods()
-            .then(budgetPeriods => {
-                setBudgetPeriods(budgetPeriods);
-                // setBudgetPeriod(findCurrentBudgetPeriod(budgetPeriods));
-                setBudgetPeriodsLoading(false);
-            })
-            .catch(() => {
-                setBudgetPeriodsError(true);
-                setBudgetPeriodsLoading(false);
-            });
-    }, [])
-
-    function getAccountNameById(id: string): string | undefined {
-        return accounts.find(element => element.id === id)?.name;
-    }
-
-    if (accountsError) {
-        return (
-            <div className="card">
-                <h1 className="card-header">!Error!</h1>
-                <p className="card-body">{accountsErrorString}</p>
-            </div>
-        );
-    }
 
     return (
         <div className="App">
-            <Navbar budgetPeriodsArray={budgetPeriods}
-                    setBudgetPeriod={setBudgetPeriod}
-                    budgetPeriodsError={budgetPeriodsError}
-                    currBudgetPeriod={budgetPeriod}
-            />
-                <Routes>
-                    <Route path="/" element={<HomePage getAccountNameById={getAccountNameById}
-                                                       accountsArray={accounts}
-                                                       budgetPeriod={budgetPeriod}
-                                                       updateBudgetPeriod={updateBudgetPeriod}
-                                                       accountsLoading={accountsLoading}
-                                                       budgetPeriodsLoading={budgetPeriodsLoading}
-                    />} />
-                    <Route path="/managebudgetperiods" element={<ManageBudgetPeriods setBudgetPeriods={setBudgetPeriods}
-                                                                                     setBudgetPeriodsError={setBudgetPeriodsError}
-                    />} />
-                </Routes>
+            <Navbar />
+            <div id="page-contents">
+                <Outlet />
+            </div>
         </div>
     );
 }

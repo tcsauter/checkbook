a04c59bf5f57e45bfc6931c352f240f1dcc9ed94
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-07-19 13:18:45 -0400

    Refactor CreditBalanceSummaryCard to take advantage of react-router data APIs.

--- src/layouts/HomePage/components/CreditBalanceSummaryCard.tsx
+++ src/layouts/HomePage/components/CreditBalanceSummaryCard.tsx
@@ -1,9 +1,9 @@
 import {AcctSummaryListItem} from "./AcctSummaryListItem";
 import {CreditBalanceSummaryCardProps} from "../../../models/props";
+import React from "react";
 
 export const CreditBalanceSummaryCard: React.FC<{
-    creditAccountSummaryArray: CreditBalanceSummaryCardProps[],
-    stillLoading: boolean
+    creditAccountSummaryArray: CreditBalanceSummaryCardProps[]
 }> = (props) => {
 
     function calculateCreditBalanceTotal(): number {
@@ -17,28 +17,22 @@ export const CreditBalanceSummaryCard: React.FC<{
             <div className='card-body'>
                 <h5 className='card-title'>Credit Balance Summary</h5>
                 <div>
-                    {props.stillLoading ?
-                        <><p>Loading...</p></>
-                        :
-                        <>
-                            <ul className='list-group mt-4'>
-                                {props.creditAccountSummaryArray.map(summaryItem => {
-                                    return <AcctSummaryListItem acctName={summaryItem.accountName}
-                                                                amt={summaryItem.amount}
-                                                                key={Math.random()}
-                                    />
-                                })}
-                            </ul>
-                            <hr/>
-                            <div className='d-flex justify-content-between'>
-                                <h5 className='card-text me-5'><b>Total:</b></h5>
-                                <p className='card-text'>{calculateCreditBalanceTotal().toLocaleString("en-US", {
-                                    style: "currency",
-                                    currency: "USD"
-                                })}</p>
-                            </div>
-                        </>
-                    }
+                    <ul className='list-group mt-4'>
+                        {props.creditAccountSummaryArray.map(summaryItem => {
+                            return <AcctSummaryListItem acctName={summaryItem.accountName}
+                                                        amt={summaryItem.amount}
+                                                        key={Math.random()}
+                            />
+                        })}
+                    </ul>
+                    <hr/>
+                    <div className='d-flex justify-content-between'>
+                        <h5 className='card-text me-5'><b>Total:</b></h5>
+                        <p className='card-text'>{calculateCreditBalanceTotal().toLocaleString("en-US", {
+                            style: "currency",
+                            currency: "USD"
+                        })}</p>
+                    </div>
                 </div>
             </div>
         </div>

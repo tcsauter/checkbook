0764f0037ca45597836d92775b74e674e3cf73df
Travis Sauter-Hunsberger <traviscsauter@gmail.com>
2023-09-03 07:35:02 -0400

    Add function to deliver information about a month for use in on-screen fields and creating a BillModel object.

--- src/utils/dateUtils.tsx
+++ src/utils/dateUtils.tsx
@@ -12,3 +12,26 @@ export function calculateDateReturnString(startDate: Date, offsetDays: number): 
 export function setInitialDateValue(): string {
     return stringifyDate(new Date());
 }
+
+export interface Month {
+    name: string;
+    num: number;
+    days: number;
+}
+
+export function getMonthArray(): Month[] {
+    return [
+        {name: "January", num: 1, days: 31},
+        {name: "February", num: 2, days: 28},
+        {name: "March", num: 3, days: 31},
+        {name: "April", num: 4, days: 30},
+        {name: "May", num: 5, days: 31},
+        {name: "June", num: 6, days: 30},
+        {name: "July", num: 7, days: 31},
+        {name: "August", num: 8, days: 31},
+        {name: "September", num: 9, days: 30},
+        {name: "October", num: 10, days: 31},
+        {name: "November", num: 11, days: 30},
+        {name: "December", num: 12, days: 31}
+    ]
+}
